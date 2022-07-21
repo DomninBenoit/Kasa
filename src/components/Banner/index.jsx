434cfd7115a580eee67e5921cc2eb944704166de
Domnin <dombouif@gmail.com>
2022-07-21 18:02:43 +0200

    update Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,13 +1,18 @@
 import './style.scss'
 import bannerHome from '../../assets/IMG.png'
-//import bannerAbout from '../../assets/about.png'
+import bannerAbout from '../../assets/about.png'
 
-function Banner() {
-  return (
+function Banner(props) {
+  return props.page === 'home' ? (
     <div className="bannerHome">
       <img src={bannerHome} alt="bannière" />
       <div className="bannerBg"></div>
       <p className="banner-text">Chez vous, partout et ailleurs</p>
+    </div>
+  ) : (
+    <div className="bannerAbout">
+      <img src={bannerAbout} alt="bannière" />
+      <div className="bannerBg"></div>
     </div>
   )
 }
