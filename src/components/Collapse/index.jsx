6ff55863a1c650d820bad9227d54f4f4add2fe39
Domnin <dombouif@gmail.com>
2022-07-24 07:49:53 +0200

    create component Collapse

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './style.scss'
+
+const Collapse = (props) => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className="collapse">
+      <div className="collapseTitle" onClick={() => setIsOpen(!isOpen)}>
+        {props.title}
+
+        <svg
+          className="svg"
+          width="16"
+          height="10"
+          viewBox="0 0 16 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {isOpen ? (
+            <path
+              d="M13.8495 9.23864L15.2079 7.86941L7.60994 0.261718L0.0120036 7.87711L1.37042 9.23864L7.60994 2.9848L13.8495 9.23864Z"
+              fill="white"
+            />
+          ) : (
+            <path
+              d="M1.3705 0.761356L0.0120851 2.13059L7.61002 9.73828L15.208 2.1229L13.8495 0.761357L7.61002 7.0152L1.3705 0.761356Z"
+              fill="white"
+            />
+          )}
+        </svg>
+      </div>
+      {isOpen && (
+        <div className="collapseText">
+          <p className="text">{props.text}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Collapse
