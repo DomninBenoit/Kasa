1ce6218b8bb740868a9ea741a0a34a8be9288739
Domnin <dombouif@gmail.com>
2022-08-01 15:13:35 +0200

    update Lodging page and Gallery components

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,17 +1,44 @@
-import GalleryItem from '../GalleryItem'
-import datas from '../../data/data.json'
+import { useState } from 'react'
+import './style.scss'
 
-const Gallery = () => {
-  const pathname = window.location.pathname
-  const split = pathname.split('/')
-  const idGal = split[2]
+const Gallery = ({ pictures, lenght }) => {
+  const [index, setIndex] = useState(0)
+
+  const onNext = () => {
+    index === lenght - 1 ? setIndex(0) : setIndex(index + 1)
+  }
+
+  const onPrevious = () => {
+    index === 0 ? setIndex(lenght - 1) : setIndex(index - 1)
+    console.log(setIndex)
+  }
   return (
     <div className="carousel">
-      {datas
-        .filter((data) => data.id.includes(idGal))
-        .map(({ pictures }) => (
-          <GalleryItem pictures={pictures} />
-        ))}
+      <img
+        src={pictures[index]}
+        alt="pictureActive"
+        className="pictureActive"
+      />
+
+      <svg
+        onClick={() => onPrevious()}
+        className="arrowLeft"
+        viewBox="0 0 48 80"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" />
+      </svg>
+      <svg
+        onClick={() => onNext()}
+        className="arrowRight"
+        viewBox="0 0 48 80"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" />
+      </svg>
+      <p className="pictureNumber">
+        {index + 1} / {lenght}
+      </p>
     </div>
   )
 }
--- src/pages/Lodging/index.jsx
+++ src/pages/Lodging/index.jsx
@@ -1,12 +1,37 @@
 import Header from '../../components/Header'
 import Gallery from '../../components/Gallery'
 import './style.scss'
+import { useParams } from 'react-router-dom'
+import datas from '../../data/data.json'
+import Host from '../../components/Host'
+import Tags from '../../components/Tags'
+import Collapse from '../../components/Collapse'
+import Rating from '../../components/Rating'
+import Footer from '../../components/Footer'
 
 const Lodging = () => {
+  const { id } = useParams()
+  const idGal = datas.find((data) => data.id === id)
   return (
     <div>
       <Header />
-      <Gallery />
+      <Gallery pictures={idGal.pictures} lenght={idGal.pictures.length} />
+      <div className="tlahr">
+        <div className="tla">
+          <p className="title">{idGal.title}</p>
+          <p className="location">{idGal.location}</p>
+          <Tags tags={idGal.tags} />
+        </div>
+        <div className="hr">
+          <Host host={idGal.host} />
+          <Rating rating={idGal.rating} />
+        </div>
+      </div>
+      <div className="collapseLodging">
+        <Collapse title="Description" text={idGal.description} />
+        <Collapse title="Équipements" text={idGal.equipments} />
+      </div>
+      <Footer />
     </div>
   )
 }
